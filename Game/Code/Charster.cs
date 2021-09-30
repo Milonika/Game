9a5a6b57841e2d558adb81e43485e3c425951088
алина алина <fkbyf-2003zv@mail.ru>
2021-09-30 10:32:27 +0300

    Добавьте файлы проекта.

---
+++ Game/Code/Charster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Code
+{
+    
+    class Charster
+    {
+        public string Name { get; private set; }
+        public int Strength { get; private set; } = 20;
+        public int Doxterity { get; private set; } = 20;
+        public int Intelegent { get; private set; } = 20;
+        public int Constituion { get; private set; } = 20;
+        
+        //public int Extra { get; private set; } = 20;
+    }
+}
